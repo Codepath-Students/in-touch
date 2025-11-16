63dd5e504833cf7344c22ce03afa0657a0188cdc
akgupta2002D <akgupta2002d@gmail.com>
2025-11-16 16:30:41 +0545

    Add connection page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing-Page.jsx";
 import AuthModal from "./components/AuthModal.jsx";
+import Connections from "./connections/ConnectionPage.jsx";
 
 function App() {
   const [authOpen, setAuthOpen] = useState(false);
@@ -17,6 +18,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<LandingPage onAuth={openAuth} />} />
+        <Route path="/connections" element={<Connections />} />
       </Routes>
       <AuthModal
         open={authOpen}
